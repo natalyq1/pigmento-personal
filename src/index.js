8c64ff121647c47be047e3709a897d1998c8e4b0
natalyq1 <onatylili@gmail.com>
2022-06-09 11:44:35 -0500

    changes on link

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import Inicio from './pages/inicio/Inicio';
 import Galeria from './pages/pagegaleria/Maingaleria';
 import Productos from './pages/pageproductos/Mainproductos';
 
-import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { BrowserRouter,Navigate,Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 < BrowserRouter>
     <Routes>
     <Route path='/' element={<Inicio/>}></Route>
-    <Route path='/inicio' element={<Inicio/>}></Route>
+    <Route path='/inicio' element={<Navigate replace to={"/"}/>}></Route>
     <Route path='/galeria' element={<Galeria/>}></Route>
     <Route path='/productos' element={<Productos/>}></Route>
       
